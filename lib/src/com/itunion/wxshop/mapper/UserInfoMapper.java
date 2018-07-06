3fbae3bcb61f583de95408d6d9f764715b8ed5e6
yundianzixun <zhenghhgz@163.com>
2018-07-06 11:21:26 +0800

    新增内容

---
+++ lib/src/com/itunion/wxshop/mapper/UserInfoMapper.java
@@ -0,0 +1,17 @@
+package com.itunion.wxshop.mapper;
+
+import com.itunion.wxshop.model.UserInfo;
+
+public interface UserInfoMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(UserInfo record);
+
+    int insertSelective(UserInfo record);
+
+    UserInfo selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(UserInfo record);
+
+    int updateByPrimaryKey(UserInfo record);
+}
---
+++ lib/src/com/itunion/wxshop/model/UserInfo.java
@@ -0,0 +1,75 @@
+package com.itunion.wxshop.model;
+
+import java.util.Date;
+
+public class UserInfo {
+    private Integer id;
+
+    private String userName;
+
+    private String password;
+
+    private String sex;
+
+    private String content;
+
+    private Date createDate;
+
+    private Date updateDate;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+
+    public Date getUpdateDate() {
+        return updateDate;
+    }
+
+    public void setUpdateDate(Date updateDate) {
+        this.updateDate = updateDate;
+    }
+}
